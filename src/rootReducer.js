30a369a73c9187dc00f98b25d5f2d3fb33bd4611
Hasier Pastor <hasier.pastor@gmail.com>
2018-11-13 17:19:42 -0800

    added discount field on cart

--- src/rootReducer.js
+++ src/rootReducer.js
@@ -1,9 +1,11 @@
 import products from './products.json';
-import { ADD_PRODUCT, REMOVE_PRODUCT } from './actionTypes';
+import { ADD_PRODUCT, REMOVE_PRODUCT, APPLY_DISCOUNT } from './actionTypes';
 
-function rootReducer(state = { items: products.items, cart: [] }, action) {
-  console.log("reducer ran; state & action:", state, action);
-
+function rootReducer(
+  state = { items: products.items, cart: [], discount: 1 },
+  action
+) {
+  console.log('reducer ran; state & action:', state, action);
 
   switch (action.type) {
     //if item is not in cart, add to cart - otherwise increment quantity by +1
@@ -13,17 +15,26 @@ function rootReducer(state = { items: products.items, cart: [] }, action) {
         // now we increment item
         let newCart = [...state.cart];
         newCart[idx].quantity++;
-        return { ...state, cart: newCart }
+        return { ...state, cart: newCart };
       }
       action.item.quantity = 1;
       return { ...state, cart: [...state.cart, action.item] };
 
     case REMOVE_PRODUCT:
-      return { ...state, cart: state.cart.filter(item => item.id !== action.id) };
+      return {
+        ...state,
+        cart: state.cart.filter(item => item.id !== action.id)
+      };
+
+    case APPLY_DISCOUNT:
+      return {
+        ...state,
+        discount: action.discount
+      };
 
     default:
       return state;
   }
 }
 
-export default rootReducer
+export default rootReducer;
